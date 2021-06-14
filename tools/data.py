dc42517373e9db1ae5e82a42c71ef9db001d0e58
YuSyuan1208 <37201761+YuSyuan1208@users.noreply.github.com>
2021-06-14 10:00:24 +0800

    [IMP]data新增自動創建資料夾

--- tools/data.py
+++ tools/data.py
@@ -2,9 +2,11 @@ import ast
 import sys
 import os
 import json
+import errno
 import logging
 
 _logger = logging.getLogger(__name__)
+
 
 class DataImport():
 
@@ -16,20 +18,21 @@ class DataImport():
     def __init__(self, path, rw_type='json', encoding='utf8') -> None:
         if path:
             self._file_path = path
+            self._file_directory = os.path.dirname(path)
         self._file_type = rw_type
         self._file_encoding = encoding
         # pass
 
     def get_file_data(self, auto_create=False):
-        rs = self.json_file(self._file_path, 'r',)
-        if not rs and auto_create:
+        rs = self.json_file(self._file_path, 'r')
+        if not rs and self._check_directory(auto_create=auto_create):
             rs = self.write_file_data()
         return rs
-    
-    def write_file_data(self, data=False):
+
+    def write_file_data(self, data=False, indent=None):
         if data:
             self._file_data = data
-        rs = self.json_file(self._file_path, 'w')
+        rs = self.json_file(self._file_path, 'w', indent=indent)
         return rs
 
     def _check_json_file(self, keys=[]):
@@ -43,7 +46,22 @@ class DataImport():
         else:
             return True
 
-    def json_file(self, file_path, action_type):
+    def _check_directory(self, auto_create=False):
+        if not os.path.exists(self._file_directory):
+            _logger.warning(f'No such directory: {self._file_directory}')
+            if auto_create:
+                try:
+                    os.makedirs(self._file_directory)
+                    _logger.info(f'{self._file_directory} created.')
+                    return True
+                except OSError as exc:  # Guard against race condition
+                    if exc.errno != errno.EEXIST:
+                        raise
+        else:
+            return True
+        return False
+
+    def json_file(self, file_path, action_type, indent=None):
         """ file control """
         if action_type == 'r':
             if os.path.isfile(file_path):
@@ -68,7 +86,7 @@ class DataImport():
             _logger.info(file_path + ' file saving.')
             with open(file_path, 'w') as ofile:
                 if self._file_type == 'json':
-                    json.dump(self._file_data, ofile)
+                    json.dump(self._file_data, ofile, indent=indent)
                 elif self._file_type == 'eval':
                     ofile.write(self._file_data)
             return self._file_data
