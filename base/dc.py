acde83974cba99c710baab2b25cef70851ea00d1
YuSyuan1208 <37201761+YuSyuan1208@users.noreply.github.com>
2021-11-23 10:57:29 +0800

    [ADD][dc]intents，新增可讀取身分組成員

--- base/dc.py
+++ base/dc.py
@@ -2,6 +2,7 @@
 # Discord
 # =========================================================================
 import logging
+import discord
 from discord.ext import commands
 from tools.data import DataImport
 from tools import config
@@ -21,11 +22,13 @@ _logger = logging.getLogger(__name__)
 #         "TOKEN": {Your bot TOKEN}
 #     }
 #     """ % CONFIG_SETTING_PATH)
+intents = discord.Intents.default()  # Allow the use of custom intents
+intents.members = True
 
 bot = commands.Bot(
-    command_prefix=config['prefix'].split(','), case_insensitive=True)
+    command_prefix=config['prefix'].split(','), case_insensitive=True, intents=intents)
 
-# base robot command 
+# base robot command
 func = dc_main_func(bot)
 bot.add_listener(func.on_ready)
 bot.add_command(commands.Command(func.load))
